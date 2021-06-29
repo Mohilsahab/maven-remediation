1bb984ae00e0081d0e667b707a4ecc3d1a7593dd
ArmorCode Dev <dev@armorcode.io>
2021-06-29 15:48:41 +0530

    Initial commit

---
+++ src/main/java/io/armorcode/remediation_sample/App.java
@@ -0,0 +1,15 @@
+package io.armorcode.remediation_sample;
+
+import org.apache.poi.ss.usermodel.Row;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Row r = null;
+        System.out.println( "Hello World!" );
+    }
+}
